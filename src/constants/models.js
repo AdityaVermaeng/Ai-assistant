3a8538536384dc1f8ef91e89faaa7851a7d4b3ad
AdityaVermaeng <adityafriendrahul@gmail.com>
2026-04-19 02:14:12 +0530

    fix: fix  some typo errros and remove spaces

--- src/constants/models.js
+++ src/constants/models.js
@@ -4,13 +4,13 @@
 //All models are free Tier options
 
 export const MODELS = [
-{ id: 'mistralai/devstral-2512 free', label:'Devstral 2',shortLabel: 'Devstral 2' },
+{ id: 'mistralai/devstral-2512:free', label:'Devstral 2',shortLabel: 'Devstral 2' },
 { id:'nex-agi/deepseek-v3.1-nex-n1:free', label: 'DeepSeek V3.1 Nex N1',shortLabel: 'DeepSeekV3.1'},
 { id: 'amazon/nova-2-lite-v1:free', label: 'Amazon Nova 2 Lite', shortLabel: 'Nova 2 Lite' },
-{ id: 'arcee-ai/trinity-mini: free', label: 'Arcee Trinity Mini', shortLabel: 'Trinity Mini' },
+{ id: 'arcee-ai/trinity-mini:free', label: 'Arcee Trinity Mini', shortLabel: 'Trinity Mini' },
 { id: 'tngtech/tng-rlt-chimera:free', label: 'TNG R1T Chimera',shortLabel: 'R1T Chimera' },
-{ id:'allenai/olmo-3-32b-think: free', label:'Olmo 3 32B Think', shortLabel:'Olmo 3 Think' },
-{ id: 'kwaipilot/kat-coder-pro: free', label: 'KAT-Coder-Pro V1', shortLabel: 'KAT-Coder-Pro' },
+{ id:'allenai/olmo-3-32b-think:free', label:'Olmo 3 32B Think', shortLabel:'Olmo 3 Think' },
+{ id: 'kwaipilot/kat-coder-pro:free', label: 'KAT-Coder-Pro V1', shortLabel: 'KAT-Coder-Pro' },
 { id: 'nvidia/nemotron-nano-12b-v2-vl:free', label: 'Nemotron Nano 12B 2 VL', shortLabel:
 'Nemotron 12B VL' },
 { id:'alibabatongyi-deepresearch-30b-a3b:free', label: 'Tongyi DeepResearch 30B', shortLabel:
